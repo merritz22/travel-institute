9659c69539b7fecdf896f261cf852376da819b2b
merritz22 <morritzngueguim43@gmail.com>
2024-04-26 11:12:05 +0100

    up

--- public/build/assets/InputLabel-c2c40302.js
+++
@@ -1,1 +0,0 @@
-import{o as e,d as t,t as o,m as n}from"./app-53b01baa.js";const l={class:"block font-medium text-sm text-gray-700"},r={key:0},c={key:1},m={__name:"InputLabel",props:{value:String},setup(s){return(a,_)=>(e(),t("label",l,[s.value?(e(),t("span",r,o(s.value),1)):(e(),t("span",c,[n(a.$slots,"default")]))]))}};export{m as _};
